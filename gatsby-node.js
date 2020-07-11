2ca33933f5cc851fb4ad8f9f8a92a2b6e65daa8a
John Untivero <jpuntivero@gmail.com>
2020-07-11 02:31:07 -0500

    node - created pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,57 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node }) => {
+  if (node.internal.type === `PedicuresJson`) {
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allProductsJson {
+          edges {
+            node {
+              id
+              type
+              products {
+                code
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allProductsJson.edges.forEach(({ node }) => {
+        const indexLink = path.join(`products`, node.type)
+        // console.log("PRODUCT TYPE!!!!!", indexLink, node)
+        // create product page
+        createPage({
+          path: indexLink,
+          component: `${__dirname}/src/templates/productList.jsx`,
+          context: {
+            id: node.id,
+          },
+        })
+
+        node.products.forEach(product => {
+          const productLink = path.join(indexLink, product.code)
+          // console.log(`PRODUCT`, productLink)
+          createPage({
+            path: productLink,
+            component: path.resolve(`src/templates/product.jsx`),
+            context: {
+              id: node.id,
+              code: product.code,
+            },
+          })
+        })
+      })
+      resolve()
+    })
+  })
+}
