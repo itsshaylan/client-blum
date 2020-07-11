ad2e3a5a1d4449e5f7fadc1f2a3284de13493a85
John Untivero <jpuntivero@gmail.com>
2020-07-11 02:34:10 -0500

    seo - added rich result

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -19,13 +19,55 @@ function SEO({ description, lang, meta, title }) {
             title
             description
             author
+            siteUrl
           }
         }
       }
     `
   )
 
+  const base = site.siteMetadata.siteUrl
+
   const metaDescription = description || site.siteMetadata.description
+
+  const rich = {
+    "@context": "https://schema.org",
+    "@type": "NailSalon",
+    "@id": base,
+    name: site.siteMetadata.title,
+    image: ["1x1.jpg", "4x3.jpg", "16x9.jpg"].map(i => `${base}/images/${i}`),
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "3560 N Maize Rd Suite 102",
+      addressLocality: "Wichita",
+      addressRegion: "KS",
+      postalCode: "67205",
+      addressCountry: "US",
+    },
+    priceRange: "$$",
+    telephone: "+13167292586",
+    currenciesAccepted: "USD",
+    OpeningHoursSpecification: [
+      {
+        "@type": "OpeningHoursSpecification",
+        dayOfWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+        opens: "09:00",
+        closes: "19:00",
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        dayOfWeek: ["Saturday"],
+        opens: "09:00",
+        closes: "18:00",
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        dayOfWeek: ["Sunday"],
+        opens: "12:00",
+        closes: "17:00",
+      },
+    ],
+  }
 
   return (
     <Helmet
@@ -67,8 +109,14 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: "@id",
+          content: base,
+        },
       ].concat(meta)}
-    />
+    >
+      <script type="application/ld+json">{JSON.stringify(rich)}</script>
+    </Helmet>
   )
 }
 
