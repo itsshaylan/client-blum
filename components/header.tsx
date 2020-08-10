7a77e493d41e38c484f91053ead924f28bc53c57
John Untivero <jpuntivero@gmail.com>
2020-08-10 15:35:35 -0500

    navbar - toggle menu

--- components/header.tsx
+++ components/header.tsx
@@ -1,40 +1,54 @@
-const Header = props => (
-  <header>
-    <div className="container">
-      <span className="header-logo">
-        <a>BLUM</a>
-        <i className="fas fa-bars toggle"></i>
-      </span>
-      <nav>
-        <ul className="nav-links">
-          <li>
-            <a href="#">home</a>
-          </li>
-          <li>
-            <a href="#">gallery</a>
-          </li>
-        </ul>
-        <ul className="nav-links">
-          <li>
-            <a href="#" className="active">
-              e-gift
-            </a>
-          </li>
-          <li>
-            <a href="#">products</a>
-          </li>
-          <li>
-            <a href="#">policy</a>
-          </li>
-          <li className="">
-            <a href="#" className="button">
-              book appointment
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-)
+import { useState } from "react"
+
+const Header = props => {
+  const [showMenu, setMenu] = useState(true)
+
+  function toggle() {
+    setMenu(!showMenu)
+  }
+
+  return (
+    <header>
+      <div className="container">
+        <span className="header-logo">
+          <a>BLUM</a>
+          <a href="#" onClick={toggle}>
+            <i className="fas fa-bars toggle"></i>
+          </a>
+        </span>
+        <nav style={{ display: showMenu ? "flex" : "none" }}>
+          <ul className="nav-links">
+            <li>
+              <a href="#" onClick={() => alert("c")}>
+                home
+              </a>
+            </li>
+            <li>
+              <a href="#">gallery</a>
+            </li>
+          </ul>
+          <ul className="nav-links">
+            <li>
+              <a href="#" className="active">
+                e-gift
+              </a>
+            </li>
+            <li>
+              <a href="#">products</a>
+            </li>
+            <li>
+              <a href="#">policy</a>
+            </li>
+            <li className="">
+              <a href="#" className="button">
+                book appointment
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
 
 export default Header
