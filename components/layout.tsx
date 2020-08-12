dd4f0d8dafe8dac55c71b67baeae20d731b5d12f
John Untivero <jpuntivero@gmail.com>
2020-08-12 14:52:57 -0500

    Layout - cleanup

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,8 +3,7 @@ import Header from "./header"
 const Layout = props => (
   <div className="page">
     <Header />
-    <div className="content">{props.children}</div>
-    <h3>Ending Portion</h3>
+    <div className="page">{props.children}</div>
   </div>
 )
 
