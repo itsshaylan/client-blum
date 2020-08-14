fe4b2169b05c3c5bb929d834b85c39548f314597
John Untivero <jpuntivero@gmail.com>
2020-08-14 14:24:36 -0500

    card - updated layout

--- components/card.tsx
+++ components/card.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/card.module.scss"
 export const CardStyle = styles
 
 type CardProps = {
+  type: string
   path?: string
   image?: string
 }
@@ -15,14 +16,16 @@ export const Card: React.FunctionComponent<CardProps> = props => {
     background: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #847577 100%),  url(${imageSrc}), #FFFFFF;
 border-radius: 16px 16px 0px 0px`,
     backgroundSize: "cover",
+    backgroundPosition: "center center",
   }
   return (
     <div className={styles.card}>
       <div className={styles.card_body} style={style}>
+        <h3>{props.type}</h3>
         {props.children}
       </div>
       <div className={styles.card_footer}>
-        <a href="#">see all manicure options</a>
+        <a href="#">see all {props.type} options</a>
       </div>
     </div>
   )
