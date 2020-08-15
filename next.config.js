5c74a9f49e774e7d16c9237a2986c9b3df6fc97c
John Untivero <jpuntivero@gmail.com>
2020-08-14 21:19:26 -0500

    config - removed assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 const withOptimizedImages = require("next-optimized-images")
 
 module.exports = withOptimizedImages({
-  assetPrefix: "https://blumnailbar.com",
+  // assetPrefix: "https://blumnailbar.com",
   webpack(config) {
     config.resolve.alias.images = path.join(__dirname, "images")
     return config
