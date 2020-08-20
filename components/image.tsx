d3fec1532bd463eb369d8da61ea278cac4110053
John Untivero <jpuntivero@gmail.com>
2020-08-20 17:37:58 -0500

    removed image functions

--- components/image.tsx
+++ components/image.tsx
@@ -4,22 +4,22 @@ type ImageProps = {
 
 export const imageSizes: number[] = [320, 640, 1080]
 
-export function imageSrc(path: string, width?: number): string {
-  const filename: string = !!path ? path : "default.jpg"
-  const size = !!width ? width : imageSizes[0]
+// export function imageSrc(path: string, width?: number): string {
+//   const filename: string = !!path ? path : "default.jpg"
+//   const size = !!width ? width : imageSizes[0]
 
-  const query: string[] = ["webp", "resize", `size=${size}`]
-  // const file: string = require(`images/${filename}?${query.join("&")}`)
-  const file: string = require(`images/${filename}?webp&resize&size=640`)
+//   const query: string[] = ["webp", "resize", `size=${size}`]
+//   // const file: string = require(`images/${filename}?${query.join("&")}`)
+//   const file: string = require(`images/${filename}?webp&resize&size=640`)
 
-  return file
-}
+//   return file
+// }
 
-export function imageSet(path: string): string {
-  const filename: string = !!path ? path : "default.jpg"
+// export function imageSet(path: string): string {
+//   const filename: string = !!path ? path : "default.jpg"
 
-  const file = require(`images/${filename}?resize&sizes[]=640&sizes[]=750&sizes[]=1080`)
-  console.log(file)
+//   const file = require(`images/${filename}?resize&sizes[]=640&sizes[]=750&sizes[]=1080`)
+//   console.log(file)
 
-  return file.srcSet
-}
+//   return file.srcSet
+// }
