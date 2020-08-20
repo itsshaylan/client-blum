49820734fd4dc17c6e8bf7958a62e9c2faf3982a
John Untivero <jpuntivero@gmail.com>
2020-08-20 17:22:50 -0500

    services updated - fixes #8
    added new images

--- components/card.tsx
+++ components/card.tsx
@@ -10,10 +10,11 @@ type CardProps = {
 export const Card: React.FunctionComponent<CardProps> = props => {
   // props.image = !!props.image ? props.image : `default.jpg`
   const imagePath = !!props.image ? props.image : `default.jpg`
-  const imageSrc = require(`images/${imagePath}?webp&resize&size=640`)
+  // const imageSrc = require(`images/${imagePath}?webp&resize&size=640`)
+  const imageSrc = `/images/services/${props.type}.jpg`
 
   const style: React.CSSProperties = {
-    background: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #847577 100%),  url(${imageSrc}), #FFFFFF; border-radius: 16px 16px 0px 0px`,
+    background: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #847577 100%),  url(${imageSrc}), #FFFFFF; border-radius: 16px`,
     backgroundSize: "cover",
     backgroundPosition: "center center",
   }
@@ -23,9 +24,8 @@ export const Card: React.FunctionComponent<CardProps> = props => {
         <h3>{props.type}</h3>
         {props.children}
       </div>
-      <div className={styles.card_footer}>
-        <a href="#">see all {props.type} options</a>
-      </div>
+      {/* <div className={styles.card_footer}>
+      </div> */}
     </div>
   )
 }
